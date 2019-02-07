e497fbb8656277e4c1391ea64a3e1dbdc56d5ca9
knokko <knokogator@hotmail.com>
2019-02-07 20:06:33 +0100

    Clean-up, add tests, add documentation and add methods to convert i8s to i16s

--- src/converter.rs
+++ src/converter.rs
@@ -1,27 +1,10 @@
-static mut POWERS: [u64; 63] = [0; 63];
-
-/**
- * Initialized static variables that are required for a part of the functionality of this library.
- * This method must be called before any functions of this library are used.
- * Calling this multiple times will not do harm, but is a waste of power and performance.
- */
-pub fn init(){
-    unsafe {
-        POWERS = create_powers();
-    }
-}
-
-fn create_powers() -> [u64; 63] {
-    let mut powers: [u64; 63] = [0; 63];
-    let mut l: u64 = 1;
-
-    for index in 0..powers.len() {
-        powers[index] = l;
-        l *= 2;
-    }
-
-    powers
-}
+const POWERS: [u64; 63] = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 
+524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 
+2147483648, 4294967296, 8589934592, 17179869184, 34359738368, 68719476736, 137438953472, 274877906944, 549755813888, 
+1099511627776, 2199023255552, 4398046511104, 8796093022208, 17592186044416, 35184372088832, 70368744177664, 140737488355328, 
+281474976710656, 562949953421312, 1125899906842624, 2251799813685248, 4503599627370496, 9007199254740992, 18014398509481984, 
+36028797018963968, 72057594037927936, 144115188075855872, 288230376151711744, 576460752303423488, 1152921504606846976, 
+2305843009213693952, 4611686018427387904];
 
 /**
  * Converts 8 booleans to an i8. This can be useful for efficiently storing boolean values because they occupy
@@ -255,4 +238,305 @@ pub fn i8_to_boolean_tuple(mut byte: i8) -> (bool, bool, bool, bool, bool, bool,
     booleans.6 = byte == 1;
 
     booleans
+}
+
+/**
+ * Converts 8 booleans to a u8. This function can be used to store multiple booleans more compactly in
+ * memory or in disk. The original booleans can be obtained by using u8_to_boolean_tuple or
+ * u8_to_boolean_array. Every unique 8-tuple of booleans will map to a unique u8.
+ */
+pub fn booleans_to_u8(b1: bool, b2: bool, b3:bool, b4:bool, b5:bool, b6:bool, b7:bool, b8:bool) -> u8 {
+    let mut result: u8 = 0;
+    if b1 {
+        result += 128;
+    }
+    if b2 {
+        result += 64;
+    }
+    if b3 {
+        result += 32;
+    }
+    if b4 {
+        result += 16;
+    }
+    if b5 {
+        result += 8;
+    }
+    if b6 {
+        result += 4;
+    }
+    if b7 {
+        result += 2;
+    }
+    if b8 {
+        result += 1;
+    }
+    result
+}
+
+/**
+ * Converts a boolean-tuple of size 8 to a u8. This function can be used to store multiple 
+ * booleans more compactly in memory or in disk. The original booleans can be obtained by 
+ * using u8_to_boolean_tuple or u8_to_boolean_array. Every unique 8-tuple of booleans will 
+ * map to a unique u8.
+ */
+pub fn boolean_tuple_to_u8(bools: (bool,bool,bool,bool,bool,bool,bool,bool)) -> u8 {
+    let mut result: u8 = 0;
+    if bools.0 {
+        result += 128;
+    }
+    if bools.1 {
+        result += 64;
+    }
+    if bools.2 {
+        result += 32;
+    }
+    if bools.3 {
+        result += 16;
+    }
+    if bools.4 {
+        result += 8;
+    }
+    if bools.5 {
+        result += 4;
+    }
+    if bools.6 {
+        result += 2;
+    }
+    if bools.7 {
+        result += 1;
+    }
+    result
+}
+
+/**
+ * Converts a boolean array of size 8 to a u8. This function can be used to store multiple 
+ * booleans more compactly in memory or in disk. The original booleans can be obtained by 
+ * using u8_to_boolean_array or u8_to_boolean_tuple. Every unique 8-tuple of booleans will 
+ * map to a unique u8.
+ */
+pub fn boolean_array_to_u8(bools: [bool; 8]) -> u8 {
+    let mut result: u8 = 0;
+    if bools[0] {
+        result += 128;
+    }
+    if bools[1] {
+        result += 64;
+    }
+    if bools[2] {
+        result += 32;
+    }
+    if bools[3] {
+        result += 16;
+    }
+    if bools[4] {
+        result += 8;
+    }
+    if bools[5] {
+        result += 4;
+    }
+    if bools[6] {
+        result += 2;
+    }
+    if bools[7] {
+        result += 1;
+    }
+    result
+}
+
+/**
+ * Converts a boolean slice of size 8 to a u8. This function can be used to store multiple 
+ * booleans more compactly in memory or in disk. The original booleans can be obtained by 
+ * using u8_to_boolean_array or u8_to_boolean_tuple. Every unique 8-tuple of booleans will 
+ * map to a unique u8.
+ */
+pub fn boolean_slice_to_u8(bools: &[bool; 8]) -> u8 {
+    let mut result: u8 = 0;
+    if bools[0] {
+        result += 128;
+    }
+    if bools[1] {
+        result += 64;
+    }
+    if bools[2] {
+        result += 32;
+    }
+    if bools[3] {
+        result += 16;
+    }
+    if bools[4] {
+        result += 8;
+    }
+    if bools[5] {
+        result += 4;
+    }
+    if bools[6] {
+        result += 2;
+    }
+    if bools[7] {
+        result += 1;
+    }
+    result
+}
+
+/**
+ * Converts a u8 to a boolean-tuple of size 8. This function can be used to convert the result
+ * of boolean_tuple_to_u8 back to the original u8. This function will map every unique u8 value
+ * to a unique boolean tuple.
+ */
+pub fn u8_to_boolean_tuple(mut byte: u8) -> (bool, bool, bool, bool, bool, bool, bool, bool) {
+    let mut result = (false,false,false,false,false,false,false,false);
+    if byte >= 128 {
+        result.0 = true;
+        byte -= 128;
+    }
+    if byte >= 64 {
+        result.1 = true;
+        byte -= 64;
+    }
+    if byte >= 32 {
+        result.2 = true;
+        byte -= 32;
+    }
+    if byte >= 16 {
+        result.3 = true;
+        byte -= 16;
+    }
+    if byte >= 8 {
+        result.4 = true;
+        byte -= 8;
+    }
+    if byte >= 4 {
+        result.5 = true;
+        byte -= 4;
+    }
+    if byte >= 2 {
+        result.6 = true;
+        byte -= 2;
+    }
+    result.7 = byte == 1;
+    result
+}
+
+/**
+ * Converts a u8 to a boolean array of size 8. This function can be used to convert the result
+ * of boolean_array_to_u8 back to the original u8. This function will map every unique u8 value
+ * to a unique boolean array.
+ */
+pub fn u8_to_boolean_array(mut byte: u8) -> [bool; 8] {
+    let mut result = [false; 8];
+    if byte >= 128 {
+        result[0] = true;
+        byte -= 128;
+    }
+    if byte >= 64 {
+        result[1] = true;
+        byte -= 64;
+    }
+    if byte >= 32 {
+        result[2] = true;
+        byte -= 32;
+    }
+    if byte >= 16 {
+        result[3] = true;
+        byte -= 16;
+    }
+    if byte >= 8 {
+        result[4] = true;
+        byte -= 8;
+    }
+    if byte >= 4 {
+        result[5] = true;
+        byte -= 4;
+    }
+    if byte >= 2 {
+        result[6] = true;
+        byte -= 2;
+    }
+    result[7] = byte == 1;
+    result
+}
+
+/**
+ * Convert 2 i8 values to an i16 value. Every distinct pair of i8 values will be mapped 
+ * to a another i16 value. This function can be used to convert the result of
+ * i16_to_i8_tuple, i16_to_i8_array or i16_to_i8_1 and i16_to_i8_2 back to the original
+ * i16 value.
+ */
+pub fn i8s_to_i16(byte1: i8, byte2: i8) -> i16 {
+    ((byte2 as i16) << 8) | ((byte1 as i16) & 0xff)
+}
+
+/**
+ * Convert a pair of i8 values to an i16 value. Every distinct pair of i8 values will be mapped 
+ * to a another i16 value. This function can be used to convert the result of
+ * i16_to_i8_tuple, i16_to_i8_array or i16_to_i8_1 and i16_to_i8_2 back to the original
+ * i16 value.
+ */
+pub fn i8_tuple_to_i16(bytes: (i8, i8)) -> i16 {
+    i8s_to_i16(bytes.0, bytes.1)
+}
+
+/**
+ * Convert an array containing 2 i8 values to a i16 value. Every distinct pair of i8 
+ * values will be mapped to a another i16 value. This function can be used to convert 
+ * the result of i16_to_i8_array, i16_to_i8_tuple or i16_to_i8_1 and i16_to_i8_2 back 
+ * the original i16 value.
+ */
+pub fn i8_array_to_i16(bytes: [i8; 2]) -> i16 {
+    i8s_to_i16(bytes[0], bytes[1])
+}
+
+/**
+ * Convert a slice containing 2 i8 values to a i16 value. Every distinct pair of i8 
+ * values will be mapped to a another i16 value. This function can be used to convert 
+ * the result of i16_to_i8_array, i16_to_i8_tuple or i16_to_i8_1 and i16_to_i8_2 back 
+ * the original i16 value.
+ */
+pub fn i8_slice_to_i16(bytes: &[i8; 2]) -> i16 {
+    i8s_to_i16(bytes[0], bytes[1])
+}
+
+/**
+ * The first function to convert an i16 value to i8 values. This function is useless
+ * without the function i16_to_i8_2, that converts the i16 value to the other
+ * i8 value. These 2 functions together will map every distinct i16 value to another
+ * pair of i8 values. The original i16 value can be restored with the function
+ * i8s_to_i16(i16_to_i8_1(original), i16_to_i8_2(original)). Similarly, the original
+ * value can be restored by using i8_tuple_to_i16 and i8_array_to_i16.
+ */
+pub fn i16_to_i8_1(int16: i16) -> i8 {
+    int16 as i8
+}
+
+/**
+ * The second function to convert an i16 value to i8 values. This function is useless
+ * without the function i16_to_i8_1, that converts the i16 value to the other
+ * i8 value. These 2 functions together will map every distinct i16 value to another
+ * pair of i8 values. The original i16 value can be restored with the function
+ * i8s_to_i16(i16_to_i8_1(original), i16_to_i8_2(original)). Similarly, the original
+ * value can be restored by using i8_tuple_to_i16, i8_array_to_i16 and i8_slice_to_i16.
+ */
+pub fn i16_to_i8_2(int16: i16) -> i8 {
+    (int16 >> 8) as i8
+}
+
+/**
+ * Converts an i16 value to a pair of i8 values. Every distinct i16 value will be mapped
+ * to another pair of i8 values. This function can be used to store an i16 value on disk
+ * or to send it over the network. The original i16 value can be restored using
+ * i8_tuple_to_i16, i8s_to_i16, i8_array_to_i16 or i8_slice_to_i16.
+ */
+pub fn i16_to_i8_tuple(int16: i16) -> (i8, i8) {
+    (i16_to_i8_1(int16), i16_to_i8_2(int16))
+}
+
+/**
+ * Converts an i16 value to an array of i8 values. Every distinct i16 value will be mapped
+ * to another array of i8 values. This function can be used to store an i16 value on disk
+ * or to send it over the network. The original i16 value can be restored using
+ * i8_array_to_i16, i8_slice_to_i16, i8s_to_i16 or i8_tuple_to_i16.
+ */
+pub fn i16_to_i8_array(int16: i16) -> [i8; 2] {
+    [i16_to_i8_1(int16), i16_to_i8_2(int16)]
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,164 +1,3 @@
-
-
-
-
-
-
-pub fn booleans_to_u8(b1: bool, b2: bool, b3:bool, b4:bool, b5:bool, b6:bool, b7:bool, b8:bool) -> u8 {
-    let mut result: u8 = 0;
-    if b1 {
-        result += 128;
-    }
-    if b2 {
-        result += 64;
-    }
-    if b3 {
-        result += 32;
-    }
-    if b4 {
-        result += 16;
-    }
-    if b5 {
-        result += 8;
-    }
-    if b6 {
-        result += 4;
-    }
-    if b7 {
-        result += 2;
-    }
-    if b8 {
-        result += 1;
-    }
-    result
-}
-
-pub fn boolean_tuple_to_u8(bools: (bool,bool,bool,bool,bool,bool,bool,bool)) -> u8 {
-    let mut result: u8 = 0;
-    if bools.0 {
-        result += 128;
-    }
-    if bools.1 {
-        result += 64;
-    }
-    if bools.2 {
-        result += 32;
-    }
-    if bools.3 {
-        result += 16;
-    }
-    if bools.4 {
-        result += 8;
-    }
-    if bools.5 {
-        result += 4;
-    }
-    if bools.6 {
-        result += 2;
-    }
-    if bools.7 {
-        result += 1;
-    }
-    result
-}
-
-pub fn boolean_array_to_u8(bools: [bool; 8]) -> u8 {
-    let mut result: u8 = 0;
-    if bools[0] {
-        result += 128;
-    }
-    if bools[1] {
-        result += 64;
-    }
-    if bools[2] {
-        result += 32;
-    }
-    if bools[3] {
-        result += 16;
-    }
-    if bools[4] {
-        result += 8;
-    }
-    if bools[5] {
-        result += 4;
-    }
-    if bools[6] {
-        result += 2;
-    }
-    if bools[7] {
-        result += 1;
-    }
-    result
-}
-
-pub fn u8_to_booleans(mut byte: u8) -> (bool, bool, bool, bool, bool, bool, bool, bool) {
-    let mut result = (false,false,false,false,false,false,false,false);
-    if byte >= 128 {
-        result.0 = true;
-        byte -= 128;
-    }
-    if byte >= 64 {
-        result.1 = true;
-        byte -= 64;
-    }
-    if byte >= 32 {
-        result.2 = true;
-        byte -= 32;
-    }
-    if byte >= 16 {
-        result.3 = true;
-        byte -= 16;
-    }
-    if byte >= 8 {
-        result.4 = true;
-        byte -= 8;
-    }
-    if byte >= 4 {
-        result.5 = true;
-        byte -= 4;
-    }
-    if byte >= 2 {
-        result.6 = true;
-        byte -= 2;
-    }
-    result.7 = byte == 1;
-    result
-}
-
-pub fn u8_to_boolean_array(mut byte: u8) -> [bool; 8] {
-    let mut result = [false; 8];
-    if byte >= 128 {
-        result[0] = true;
-        byte -= 128;
-    }
-    if byte >= 64 {
-        result[1] = true;
-        byte -= 64;
-    }
-    if byte >= 32 {
-        result[2] = true;
-        byte -= 32;
-    }
-    if byte >= 16 {
-        result[3] = true;
-        byte -= 16;
-    }
-    if byte >= 8 {
-        result[4] = true;
-        byte -= 8;
-    }
-    if byte >= 4 {
-        result[5] = true;
-        byte -= 4;
-    }
-    if byte >= 2 {
-        result[6] = true;
-        byte -= 2;
-    }
-    result[7] = byte == 1;
-    result
-}
-
 pub fn i8s_to_i32(byte1: i8, byte2: i8, byte3: i8, byte4: i8) -> i32 {
     (((byte4 as i32) << 24) | ((byte3 as i32 & 0xff) << 16) | ((byte2 as i32 & 0xff) << 8) | ((byte1 as i32 & 0xff)))
 }
@@ -188,8 +27,7 @@ mod tests {
 
     #[test]
     fn int8s_to_booleans() {
-        init();
-        for element in -128i8..127i8 {
+        for element in -128i8..=127i8 {
             let boolean_tuple = i8_to_boolean_tuple(element);
             let boolean_array = i8_to_boolean_array(element);
 
@@ -211,6 +49,63 @@ mod tests {
             assert_eq!(element, reverted6);
             assert_eq!(element, reverted7);
             assert_eq!(element, reverted8);
+        }
+    }
+
+    #[test]
+    fn uint8s_to_booleans() {
+        let mut l: u64 = 1;
+        for _ in 0..63 {
+            print!("{}, ", l);
+            l *= 2;
+        }
+        for element in 0u8..=255 {
+            let boolean_tuple = u8_to_boolean_tuple(element);
+            let boolean_array = u8_to_boolean_array(element);
+
+            let reverted1 = boolean_tuple_to_u8(boolean_tuple);
+            let reverted2 = booleans_to_u8(boolean_tuple.0, boolean_tuple.1, boolean_tuple.2, boolean_tuple.3, boolean_tuple.4, boolean_tuple.5, boolean_tuple.6, boolean_tuple.7);
+            let reverted3 = boolean_array_to_u8([boolean_tuple.0, boolean_tuple.1, boolean_tuple.2, boolean_tuple.3, boolean_tuple.4, boolean_tuple.5, boolean_tuple.6, boolean_tuple.7]);
+            let reverted4 = boolean_slice_to_u8(&[boolean_tuple.0, boolean_tuple.1, boolean_tuple.2, boolean_tuple.3, boolean_tuple.4, boolean_tuple.5, boolean_tuple.6, boolean_tuple.7]);
+
+            let reverted5 = boolean_tuple_to_u8((boolean_array[0], boolean_array[1], boolean_array[2], boolean_array[3], boolean_array[4], boolean_array[5], boolean_array[6], boolean_array[7]));
+            let reverted6 = booleans_to_u8(boolean_array[0], boolean_array[1], boolean_array[2], boolean_array[3], boolean_array[4], boolean_array[5], boolean_array[6], boolean_array[7]);
+            let reverted7 = boolean_array_to_u8(boolean_array);
+            let reverted8 = boolean_slice_to_u8(&boolean_array);
+
+            assert_eq!(element, reverted1);
+            assert_eq!(element, reverted2);
+            assert_eq!(element, reverted3);
+            assert_eq!(element, reverted4);
+            assert_eq!(element, reverted5);
+            assert_eq!(element, reverted6);
+            assert_eq!(element, reverted7);
+            assert_eq!(element, reverted8);
+        }
+    }
+
+    #[test]
+    fn test_i8_to_i16() {
+        for short in -32768i16..=32767 {
+            let byte1 = i16_to_i8_1(short);
+            let byte2 = i16_to_i8_2(short);
+            let pair = i16_to_i8_tuple(short);
+            let array = i16_to_i8_array(short);
+
+            assert_eq!(byte1, pair.0);
+            assert_eq!(byte2, pair.1);
+            assert_eq!(byte1, array[0]);
+            assert_eq!(byte2, array[1]);
+
+            let reverted1 = i8s_to_i16(byte1, byte2);
+            let reverted2 = i8_tuple_to_i16(pair);
+            let reverted3 = i8_array_to_i16(array);
+            let reverted4 = i8_slice_to_i16(&array);
+
+            assert_eq!(short, reverted1);
+            assert_eq!(short, reverted2);
+            assert_eq!(short, reverted3);
+            assert_eq!(short, reverted4);
         }
     }
 }
