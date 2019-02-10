817b40fcb1dd8277a86a873c3f35432e9d007e1a
knokko <knokogator@hotmail.com>
2019-02-10 15:47:45 +0100

    Work on BitOutput

--- src/output.rs
+++ src/output.rs
@@ -98,23 +98,246 @@ pub trait BitOutput {
     }
 
     /**
+     * Add the length of the i8 slice and the values of all i8s in the slice without
+     * checking the capacity. The mirror function of this function is read_i8_array. There is 
+     * no read_i8_slice because it doesn't really make sense to borrow the data since the 
+     * BitInput will create the array and won't need it for itself.
+     * 
+     * The length will be stored as i32 to make sure the stored data can also be read by 
+     * java or javascript applications that use the BitHelper variant for their language.
+     */
+    fn add_direct_i8_slice(&mut self, bytes: &[i8]){
+        self.add_direct_i32(bytes.len() as i32);
+        self.add_direct_i8s(bytes);
+    }
+
+    /**
+     * Add the provided i16 value to this BitOutput without checking the capacity. The mirror function
+     * of this function is read_i16.
+     */
+    fn add_direct_i16(&mut self, integer: i16){
+        self.add_direct_i8(i16_to_i8_1(integer));
+        self.add_direct_i8(i16_to_i8_2(integer));
+    }
+
+    /**
+     * Add all i16 values in the provided slice to this BitOutput without checking the capacity.
+     * A call to this function is equivalent to adding all i16 values directly one by one. The length
+     * of the slice is not stored, so make sure the application always knows how many i16 values
+     * to read. The mirror function of this function is read_i16s and notice that it needs the amount
+     * of i16 values to read as parameter.
+     * 
+     * If you want to store the length as well, use add_direct_i16_slice instead.
+     */
+    fn add_direct_i16s(&mut self, values: &[i16]){
+        for value in values {
+            self.add_direct_i16(*value);
+        }
+    }
+
+    /**
+     * Add the length of the i16 slice and the values of all i16s in the slice without
+     * checking the capacity. The mirror function of this function is read_i16_array. There is 
+     * no read_i16_slice because it doesn't really make sense to borrow the data since the 
+     * BitInput will create the array and won't need it for itself.
+     * 
+     * The length will be stored as i32 to make sure the stored data can also be read by 
+     * java or javascript applications that use the BitHelper variant for their language.
+     */
+    fn add_direct_i16_slice(&mut self, values: &[i16]){
+        self.add_direct_i32(values.len() as i32);
+        self.add_direct_i16s(values);
+    }
+
+    /**
      * Add the provided i32 value to this BitOutput without checking the capacity. The mirror function
      * of this function is read_i32.
      */
     fn add_direct_i32(&mut self, integer: i32){
-        let bytes = i32_to_i8_tuple(integer);
-        self.add_direct_i8(bytes.0);
-        self.add_direct_i8(bytes.1);
-        self.add_direct_i8(bytes.2);
-        self.add_direct_i8(bytes.3);
+        self.add_direct_i8(i32_to_i8_1(integer));
+        self.add_direct_i8(i32_to_i8_2(integer));
+        self.add_direct_i8(i32_to_i8_3(integer));
+        self.add_direct_i8(i32_to_i8_4(integer));
+    }
+
+    /**
+     * Add all i32 values in the provided slice to this BitOutput without checking the capacity.
+     * A call to this function is equivalent to adding all i32 values directly one by one. The length
+     * of the slice is not stored, so make sure the application always knows how many i32 values
+     * to read. The mirror function of this function is read_i32s and notice that it needs the amount
+     * of i32 values to read as parameter.
+     * 
+     * If you want to store the length as well, use add_direct_i32_slice instead.
+     */
+    fn add_direct_i32s(&mut self, values: &[i32]){
+        for value in values {
+            self.add_direct_i32(*value);
+        }
+    }
+
+    /**
+     * Add the length of the i32 slice and the values of all i32s in the slice without
+     * checking the capacity. The mirror function of this function is read_i32_array. There is 
+     * no read_i32_slice because it doesn't really make sense to borrow the data since the 
+     * BitInput will create the array and won't need it for itself.
+     * 
+     * The length will be stored as i32 to make sure the stored data can also be read by 
+     * java or javascript applications that use the BitHelper variant for their language.
+     */
+    fn add_direct_i32_slice(&mut self, values: &[i32]){
+        self.add_direct_i32(values.len() as i32);
+        self.add_direct_i32s(values);
+    }
+
+    /**
+     * Add a bool value to this BitOutput. The mirror function of this function is read_bool.
+     */
+    fn add_bool(&mut self, value: bool){
+        self.ensure_extra_capacity(1);
+        self.add_direct_bool(value);
+    }
+
+    /**
+     * Add all provided booleans to this BitOutput. This is a little faster than adding all values one by one
+     * because the capacity only needs to be checked once. The amount of booleans is NOT stored, so make sure your
+     * application knows how many bools were stored. The mirror function of this function is read_bools and the
+     * amount of bools to read must be stored as parameter.
+     * 
+     * If you want to store the amount as well, use add_bool_slice instead.
+     */
+    fn add_bools(&mut self, values: &[bool]){
+        self.ensure_extra_capacity(values.len());
+        self.add_direct_bools(values);
+    }
+
+    /**
+     * Add the length of the boolean slice and the values of all booleans in the slice. The 
+     * mirror function of this function is read_bool_array. There is no read_bool_slice because 
+     * it doesn't really make sense to borrow the data since the BitInput will create the array 
+     * and won't need it for itself.
+     * 
+     * The length will be stored as i32 to make sure the stored data can also be read by 
+     * java or javascript applications that use the BitHelper variant for their language.
+     */
+    fn add_bool_slice(&mut self, values: &[bool]){
+        self.ensure_extra_capacity(32 + values.len());
+        self.add_direct_bool_slice(values);
+    }
+
+    /**
+     * Add an i8 value to this BitOutput. The mirror function of this function is read_i8.
+     */
+    fn add_i8(&mut self, value: i8){
+        self.ensure_extra_capacity(8);
+        self.add_direct_i8(value);
+    }
+
+    /**
+     * Add all i8 values in the provided slice to this BitOutput.
+     * A call to this function is faster than adding all i8 values one by one because the capacity
+     * only needs to be checked once. The length of the slice is not stored, so make sure the 
+     * application always knows how many i8 values to read. The mirror function of this function 
+     * is read_i8s and notice that it needs the amount of i8 values to read as parameter.
+     * 
+     * If you want to store the length as well, use add_i8_slice instead.
+     */
+    fn add_i8s(&mut self, values: &[i8]){
+        self.ensure_extra_capacity(values.len() * 8);
+        self.add_direct_i8s(values);
+    }
+
+    /**
+     * Add the length of the i8 slice and the values of all i8s in the slice. The mirror 
+     * function of this function is read_i8_array. There is no read_i8_slice because it 
+     * doesn't really make sense to borrow the data since the BitInput will create the 
+     * array and won't need it for itself.
+     * 
+     * The length will be stored as i32 to make sure the stored data can also be read by 
+     * java or javascript applications that use the BitHelper variant for their language.
+     */
+    fn add_i8_slice(&mut self, values: &[i8]){
+        self.ensure_extra_capacity(32 + values.len() * 8);
+        self.add_direct_i8_slice(values);
+    }
+
+    /**
+     * Add an i16 value to this BitOutput. The mirror function of this function is read_i16.
+     */
+    fn add_i16(&mut self, value: i16){
+        self.ensure_extra_capacity(16);
+        self.add_direct_i16(value);
+    }
+
+    /**
+     * Add all i16 values in the provided slice to this BitOutput.
+     * A call to this function is faster than adding all i16 values one by one because the capacity
+     * only needs to be checked once. The length of the slice is not stored, so make sure the 
+     * application always knows how many i16 values to read. The mirror function of this function 
+     * is read_i16s and notice that it needs the amount of i16 values to read as parameter.
+     * 
+     * If you want to store the length as well, use add_i16_slice instead.
+     */
+    fn add_i16s(&mut self, values: &[i16]){
+        self.ensure_extra_capacity(values.len() * 16);
+        self.add_direct_i16s(values);
+    }
+
+    /**
+     * Add the length of the i16 slice and the values of all i16s in the slice. The mirror 
+     * function of this function is read_i16_array. There is no read_i16_slice because it 
+     * doesn't really make sense to borrow the data since the BitInput will create the 
+     * array and won't need it for itself.
+     * 
+     * The length will be stored as i32 to make sure the stored data can also be read by 
+     * java or javascript applications that use the BitHelper variant for their language.
+     */
+    fn add_i16_slice(&mut self, values: &[i16]){
+        self.ensure_extra_capacity(32 + values.len() * 16);
+        self.add_direct_i16_slice(values);
+    }
+
+    /**
+     * Add an i32 value to this BitOutput. The mirror function of this function is read_i32.
+     */
+    fn add_i32(&mut self, value: i32){
+        self.ensure_extra_capacity(32);
+        self.add_direct_i32(value);
+    }
+
+    /**
+     * Add all i32 values in the provided slice to this BitOutput.
+     * A call to this function is faster than adding all i32 values one by one because the capacity
+     * only needs to be checked once. The length of the slice is not stored, so make sure the 
+     * application always knows how many i32 values to read. The mirror function of this function 
+     * is read_i32s and notice that it needs the amount of i32 values to read as parameter.
+     * 
+     * If you want to store the length as well, use add_i32_slice instead.
+     */
+    fn add_i32s(&mut self, values: &[i32]){
+        self.ensure_extra_capacity(values.len() * 32);
+        self.add_direct_i32s(values);
+    }
+
+    /**
+     * Add the length of the i32 slice and the values of all i32s in the slice. The mirror 
+     * function of this function is read_i32_array. There is no read_i32_slice because it 
+     * doesn't really make sense to borrow the data since the BitInput will create the 
+     * array and won't need it for itself.
+     * 
+     * The length will be stored as i32 to make sure the stored data can also be read by 
+     * java or javascript applications that use the BitHelper variant for their language.
+     */
+    fn add_i32_slice(&mut self, values: &[i32]){
+        self.ensure_extra_capacity(32 + values.len() * 32);
+        self.add_direct_i32_slice(values);
     }
 }
 
-pub struct BoolArrayBitOutput {
+pub struct BoolVecBitOutput {
     vector: Vec<bool>
 }
 
-impl BitOutput for BoolArrayBitOutput {
+impl BitOutput for BoolVecBitOutput {
 
     fn add_direct_bool(&mut self, value: bool){
         self.vector.push(value);
@@ -133,16 +356,16 @@ impl BitOutput for BoolArrayBitOutput {
     }
 }
 
-impl BoolArrayBitOutput {
+impl BoolVecBitOutput {
 
-    pub fn new(initial_capacity: usize) -> BoolArrayBitOutput{
-        BoolArrayBitOutput {
+    pub fn new(initial_capacity: usize) -> BoolVecBitOutput{
+        BoolVecBitOutput {
             vector: Vec::with_capacity(initial_capacity)
         }
     }
 }
 
-impl std::fmt::Debug for BoolArrayBitOutput {
+impl std::fmt::Debug for BoolVecBitOutput {
 
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "BoolArrayBitOutput({:?} with capacity {})", self.vector, self.vector.capacity())
